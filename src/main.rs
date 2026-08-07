39e6516765838aac975377a287b01bc79154c647
Haksell <axel.brisse@gmail.com>
2026-08-07 08:52:58 +0200

    testing code in mod tests + start renaming nodes to rooms

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,20 @@ use itertools::Itertools as _;
 use std::{
     collections::{HashMap, HashSet, VecDeque},
     io::BufRead as _,
-    iter::zip,
-    sync::LazyLock,
 };
 
 type Path = Vec<usize>;
 type Edge = (usize, usize);
 
 #[derive(Clone, Debug, PartialEq)]
-struct Node {
+struct Room {
     name: String,
     x: i64,
     y: i64,
 }
 
 // TODO: better constructor names
-impl Node {
+impl Room {
     fn new(name: impl Into<String>, x: i64, y: i64) -> Self {
         Self { name: name.into(), x, y }
     }
@@ -54,7 +52,7 @@ impl From<std::io::Error> for ParseMapError {
 #[derive(Clone, Debug, PartialEq)]
 struct Map {
     ants: u32, // TODO: check not 0
-    nodes: Vec<Node>,
+    rooms: Vec<Room>,
     edges: Vec<Vec<usize>>,
     start: usize,
     end: usize,
@@ -62,24 +60,23 @@ struct Map {
 
 impl Map {
     fn parse() -> Result<Self, ParseMapError> {
-        enum SpecialNode {
+        enum SpecialRoom {
             Start,
             End,
         }
 
         enum ParsingState {
             Ants,
-            Nodes,
-            SpecialNode(SpecialNode),
+            Rooms,
+            SpecialRoom(SpecialRoom),
             Edges,
         }
 
         let mut parsing_state = ParsingState::Ants;
-
-        let mut node_indices = HashMap::new();
+        let mut room_indices = HashMap::new();
 
         let mut ants = None;
-        let mut nodes = Vec::new();
+        let mut rooms = Vec::new();
         let mut edges = Vec::new();
         let mut start = None;
         let mut end = None;
@@ -96,32 +93,32 @@ impl Map {
             match parsing_state {
                 ParsingState::Ants => {
                     ants = Some(Self::parse_ants(line)?);
-                    parsing_state = ParsingState::Nodes;
+                    parsing_state = ParsingState::Rooms;
                 }
-                ParsingState::Nodes => match line {
+                ParsingState::Rooms => match line {
                     "##start" => {
                         if start.is_some() {
                             return Err(ParseMapError::MultipleStartNodes);
                         }
-                        parsing_state = ParsingState::SpecialNode(SpecialNode::Start);
+                        parsing_state = ParsingState::SpecialRoom(SpecialRoom::Start);
                     }
                     "##end" => {
                         if end.is_some() {
                             return Err(ParseMapError::MultipleEndNodes);
                         }
-                        parsing_state = ParsingState::SpecialNode(SpecialNode::End);
+                        parsing_state = ParsingState::SpecialRoom(SpecialRoom::End);
                     }
                     line if line.starts_with("##") => {
                         return Err(ParseMapError::InvalidTag(line.into()));
                     }
-                    _ => match Self::parse_node(line, &node_indices) {
-                        Ok(node) => {
-                            node_indices.insert(node.name.clone(), nodes.len());
-                            nodes.push(node);
+                    _ => match Self::parse_node(line, &room_indices) {
+                        Ok(room) => {
+                            room_indices.insert(room.name.clone(), rooms.len());
+                            rooms.push(room);
                         }
-                        Err(err_parse_node) => match Self::parse_edge(line, &node_indices) {
+                        Err(err_parse_node) => match Self::parse_edge(line, &room_indices) {
                             Ok((node1, node2)) => {
-                                edges = vec![vec![]; nodes.len()];
+                                edges = vec![vec![]; rooms.len()];
                                 edges[node1].push(node2);
                                 edges[node2].push(node1);
                                 parsing_state = ParsingState::Edges;
@@ -131,19 +128,19 @@ impl Map {
                         },
                     },
                 },
-                ParsingState::SpecialNode(special_node) => {
-                    let node = Self::parse_node(line, &node_indices)?;
-                    let special_node_idx = Some(nodes.len());
+                ParsingState::SpecialRoom(special_node) => {
+                    let room = Self::parse_node(line, &room_indices)?;
+                    let special_node_idx = Some(rooms.len());
                     match special_node {
-                        SpecialNode::Start => start = special_node_idx,
-                        SpecialNode::End => end = special_node_idx,
+                        SpecialRoom::Start => start = special_node_idx,
+                        SpecialRoom::End => end = special_node_idx,
                     }
-                    node_indices.insert(node.name.clone(), nodes.len());
-                    nodes.push(node);
-                    parsing_state = ParsingState::Nodes;
+                    room_indices.insert(room.name.clone(), rooms.len());
+                    rooms.push(room);
+                    parsing_state = ParsingState::Rooms;
                 }
                 ParsingState::Edges => {
-                    let (node1, node2) = Self::parse_edge(line, &node_indices)?;
+                    let (node1, node2) = Self::parse_edge(line, &room_indices)?;
                     edges[node1].push(node2);
                     edges[node2].push(node1);
                 }
@@ -153,7 +150,7 @@ impl Map {
         let Some(ants) = ants else {
             return Err(ParseMapError::MissingAntsNumber);
         };
-        if nodes.is_empty() {
+        if rooms.is_empty() {
             return Err(ParseMapError::MissingNodes);
         }
         let Some(start) = start else {
@@ -170,18 +167,18 @@ impl Map {
             neighbors.sort_unstable();
         }
 
-        Ok(Self { ants, nodes, edges, start, end })
+        Ok(Self { ants, rooms, edges, start, end })
     }
 
     fn parse_ants(line: &str) -> Result<u32, ParseMapError> {
         line.parse::<u32>().map_err(|_err| ParseMapError::InvalidAntsNumber(line.into()))
     }
 
-    // TODO: '-' forbidden in node name or starting with 'L'
+    // TODO: '-' forbidden in room name or starting with 'L'
     fn parse_node(
         line: &str,
         node_indices: &HashMap<String, usize>,
-    ) -> Result<Node, ParseMapError> {
+    ) -> Result<Room, ParseMapError> {
         let parts = line.split_whitespace().collect_vec();
         let &[name, coord_x, coord_y] = parts.as_slice() else {
             return Err(ParseMapError::InvalidNodeLine(line.into()));
@@ -203,7 +200,7 @@ impl Map {
             return Err(ParseMapError::InvalidNodeCoordinate(line.into(), 'y', coord_y.into()));
         };
 
-        Ok(Node::new(name, coord_x, coord_y))
+        Ok(Room::new(name, coord_x, coord_y))
     }
 
     fn parse_edge(
@@ -226,75 +223,16 @@ impl Map {
     }
 }
 
-static MAP_SUBJECT_1: LazyLock<Map> = LazyLock::new(|| Map {
-    ants: 3,
-    nodes: vec![
-        Node::new("0", 1, 0),
-        Node::new("1", 5, 0),
-        Node::new("2", 9, 0),
-        Node::new("3", 13, 0),
-    ],
-    edges: vec![vec![2], vec![3], vec![0, 3], vec![1, 2]],
-    start: 0,
-    end: 1,
-});
-
-static MAP_SUBJECT_2_2: LazyLock<Map> = LazyLock::new(|| Map {
-    ants: 2,
-    nodes: vec![
-        Node::new("1", 0, 2),
-        Node::new("0", 2, 0),
-        Node::new("4", 2, 6),
-        Node::new("2", 4, 2),
-        Node::new("3", 4, 4),
-    ],
-    edges: vec![vec![1, 2], vec![0, 3], vec![0, 4], vec![1, 4], vec![2, 3]],
-    start: 1,
-    end: 2,
-});
-
-static MAP_SUBJECT_2_3: LazyLock<Map> = LazyLock::new(|| {
-    let mut map = MAP_SUBJECT_2_2.clone();
-    map.ants = 3;
-    map
-});
-
-static MAP_SUBJECT_3: LazyLock<Map> = LazyLock::new(|| Map {
-    ants: 4,
-    nodes: vec![
-        Node::new("3", 2, 2),
-        Node::new("start", 4, 0),
-        Node::new("end", 4, 6),
-        Node::new("4", 0, 4),
-        Node::new("1", 4, 2),
-        Node::new("2", 4, 4),
-        Node::new("5", 8, 2),
-        Node::new("6", 8, 4),
-    ],
-    edges: vec![
-        vec![1, 3],
-        vec![0, 4],
-        vec![5, 7],
-        vec![0, 5],
-        vec![1, 5, 6],
-        vec![2, 3, 4],
-        vec![4, 7],
-        vec![2, 6],
-    ],
-    start: 1,
-    end: 2,
-});
-
 fn reconstruct_path(parents: &[Option<(usize, usize)>], start: usize, end: usize) -> Path {
     let mut path = vec![end];
-    let mut node = end;
+    let mut room = end;
     let mut time = 0;
-    while node != start {
-        let (previous_node, previous_time) = parents[node].unwrap();
+    while room != start {
+        let (previous_node, previous_time) = parents[room].unwrap();
         for _ in previous_time..time {
-            path.push(node);
+            path.push(room);
         }
-        node = previous_node;
+        room = previous_node;
         time = previous_time;
     }
     for _ in 0..time {
@@ -305,12 +243,12 @@ fn reconstruct_path(parents: &[Option<(usize, usize)>], start: usize, end: usize
 }
 
 fn bfs(map: &Map, used_nodes_by_time: &[HashSet<usize>]) -> Path {
-    let mut parents = vec![None; map.nodes.len()];
+    let mut parents = vec![None; map.rooms.len()];
     parents[map.start] = Some((map.start, 0));
     let mut queue = VecDeque::from([(map.start, 0)]);
 
-    while let Some((node, time)) = queue.pop_front() {
-        for &neighbor in &map.edges[node] {
+    while let Some((room, time)) = queue.pop_front() {
+        for &neighbor in &map.edges[room] {
             if parents[neighbor].is_some()
                 || used_nodes_by_time
                     .get(time + 1)
@@ -319,13 +257,13 @@ fn bfs(map: &Map, used_nodes_by_time: &[HashSet<usize>]) -> Path {
                 continue;
             }
 
-            parents[neighbor] = Some((node, time + 1));
+            parents[neighbor] = Some((room, time + 1));
             if neighbor == map.end {
                 return reconstruct_path(&parents, map.start, map.end);
             }
             queue.push_back((neighbor, time + 1));
         }
-        queue.push_back((node, time + 1));
+        queue.push_back((room, time + 1));
     }
 
     unreachable!("TODO: handle disconnected graph")
@@ -351,87 +289,22 @@ fn repeated_bfs(map: &Map) -> Vec<Path> {
     paths
 }
 
-/// Exponential but provably optimal solver to compare against faster solvers.
-fn iterative_deepening_search(map: &Map) -> Vec<Path> {
-    fn valid_next_nodes(next_nodes: &[usize], map: &Map) -> bool {
-        let mut seen = HashSet::new();
-
-        for next_node in next_nodes {
-            if seen.contains(next_node) {
-                return false;
-            }
-            if *next_node != map.end && *next_node != map.start {
-                seen.insert(next_node);
-            }
-        }
-
-        true
-    }
-
-    fn dfs(map: &Map, paths: &mut [Path], depth: u32) -> bool {
-        if depth == 0 {
-            return paths.iter().all(|path| path[path.len() - 1] == map.end);
-        }
-
-        let (remaining_ants, remaining_nodes): (Vec<_>, Vec<_>) = paths
-            .iter()
-            .enumerate()
-            .map(|(ant, path)| (ant, path[path.len() - 1]))
-            .filter(|(_, node)| *node != map.end)
-            .unzip();
-
-        for next_nodes in remaining_nodes
-            .iter()
-            .map(|&node| {
-                let mut next_nodes = map.edges[node].clone();
-                next_nodes.push(node);
-                next_nodes
-            })
-            .multi_cartesian_product()
-            .filter(|next_nodes| valid_next_nodes(next_nodes, map))
-        {
-            for (&ant, next_node) in zip(&remaining_ants, next_nodes) {
-                paths[ant].push(next_node);
-            }
-
-            if dfs(map, paths, depth - 1) {
-                return true;
-            }
-
-            for &ant in &remaining_ants {
-                paths[ant].pop();
-            }
-        }
-
-        false
-    }
-
-    for max_depth in 1.. {
-        let mut paths = vec![vec![map.start]; map.ants as usize];
-        if dfs(map, &mut paths, max_depth) {
-            return paths;
-        }
-    }
-
-    unreachable!()
-}
-
 fn print_moves(map: &Map, paths: &[Path]) {
     let max_time = paths.iter().map(Vec::len).max().unwrap();
     let mut previous = vec![map.start; paths.len()];
     for time in 1..max_time {
         let mut first_in_line = true;
         for (ant, path) in paths.iter().enumerate() {
-            let Some(&node) = path.get(time) else {
+            let Some(&room) = path.get(time) else {
                 continue;
             };
-            if node != previous[ant] {
-                previous[ant] = node;
+            if room != previous[ant] {
+                previous[ant] = room;
                 print!(
                     "{}L{}-{}",
                     if first_in_line { "" } else { " " },
                     ant + 1,
-                    map.nodes[node].name
+                    map.rooms[room].name
                 );
                 first_in_line = false;
             }
@@ -446,13 +319,140 @@ fn main() {
         eprintln!("Error: {err:?}"); // TODO: Debug -> Display
         std::process::exit(1);
     });
+    let paths = repeated_bfs(&map);
+    print_moves(&map, &paths);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::{iter::zip, sync::LazyLock};
 
     type Solver = fn(&Map) -> Vec<Path>;
+
+    static MAP_SUBJECT_1: LazyLock<Map> = LazyLock::new(|| Map {
+        ants: 3,
+        rooms: vec![
+            Room::new("0", 1, 0),
+            Room::new("1", 5, 0),
+            Room::new("2", 9, 0),
+            Room::new("3", 13, 0),
+        ],
+        edges: vec![vec![2], vec![3], vec![0, 3], vec![1, 2]],
+        start: 0,
+        end: 1,
+    });
+
+    static MAP_SUBJECT_2_2: LazyLock<Map> = LazyLock::new(|| Map {
+        ants: 2,
+        rooms: vec![
+            Room::new("1", 0, 2),
+            Room::new("0", 2, 0),
+            Room::new("4", 2, 6),
+            Room::new("2", 4, 2),
+            Room::new("3", 4, 4),
+        ],
+        edges: vec![vec![1, 2], vec![0, 3], vec![0, 4], vec![1, 4], vec![2, 3]],
+        start: 1,
+        end: 2,
+    });
+
+    static MAP_SUBJECT_2_3: LazyLock<Map> = LazyLock::new(|| {
+        let mut map = MAP_SUBJECT_2_2.clone();
+        map.ants = 3;
+        map
+    });
+
+    static MAP_SUBJECT_3: LazyLock<Map> = LazyLock::new(|| Map {
+        ants: 4,
+        rooms: vec![
+            Room::new("3", 2, 2),
+            Room::new("start", 4, 0),
+            Room::new("end", 4, 6),
+            Room::new("4", 0, 4),
+            Room::new("1", 4, 2),
+            Room::new("2", 4, 4),
+            Room::new("5", 8, 2),
+            Room::new("6", 8, 4),
+        ],
+        edges: vec![
+            vec![1, 3],
+            vec![0, 4],
+            vec![5, 7],
+            vec![0, 5],
+            vec![1, 5, 6],
+            vec![2, 3, 4],
+            vec![4, 7],
+            vec![2, 6],
+        ],
+        start: 1,
+        end: 2,
+    });
+
+    /// Exponential but provably optimal solver to compare against faster solvers.
+    fn iterative_deepening_search(map: &Map) -> Vec<Path> {
+        fn valid_next_nodes(next_nodes: &[usize], map: &Map) -> bool {
+            let mut seen = HashSet::new();
+
+            for next_node in next_nodes {
+                if seen.contains(next_node) {
+                    return false;
+                }
+                if *next_node != map.end && *next_node != map.start {
+                    seen.insert(next_node);
+                }
+            }
+
+            true
+        }
+
+        fn dfs(map: &Map, paths: &mut [Path], depth: u32) -> bool {
+            if depth == 0 {
+                return paths.iter().all(|path| path[path.len() - 1] == map.end);
+            }
+
+            let (remaining_ants, remaining_nodes): (Vec<_>, Vec<_>) = paths
+                .iter()
+                .enumerate()
+                .map(|(ant, path)| (ant, path[path.len() - 1]))
+                .filter(|(_, room)| *room != map.end)
+                .unzip();
+
+            for next_nodes in remaining_nodes
+                .iter()
+                .map(|&room| {
+                    let mut next_nodes = map.edges[room].clone();
+                    next_nodes.push(room);
+                    next_nodes
+                })
+                .multi_cartesian_product()
+                .filter(|next_nodes| valid_next_nodes(next_nodes, map))
+            {
+                for (&ant, next_node) in zip(&remaining_ants, next_nodes) {
+                    paths[ant].push(next_node);
+                }
+
+                if dfs(map, paths, depth - 1) {
+                    return true;
+                }
+
+                for &ant in &remaining_ants {
+                    paths[ant].pop();
+                }
+            }
+
+            false
+        }
+
+        for max_depth in 1.. {
+            let mut paths = vec![vec![map.start]; map.ants as usize];
+            if dfs(map, &mut paths, max_depth) {
+                return paths;
+            }
+        }
+
+        unreachable!()
+    }
 
     fn solves(map: &Map, paths: &[Path]) -> bool {
         let max_path = paths.iter().map(Vec::len).max().unwrap();
@@ -460,19 +460,19 @@ mod tests {
         for time in 1..max_path {
             let mut seen = HashSet::new();
             for (ant, path) in paths.iter().enumerate() {
-                let Some(&node) = path.get(time) else {
+                let Some(&room) = path.get(time) else {
                     continue;
                 };
-                if seen.contains(&node) {
+                if seen.contains(&room) {
                     return false;
                 }
-                if node != ants[ant] && !map.edges[ants[ant]].contains(&node) {
+                if room != ants[ant] && !map.edges[ants[ant]].contains(&room) {
                     return false;
                 }
-                if node != map.end && node != map.start {
-                    seen.insert(node);
+                if room != map.end && room != map.start {
+                    seen.insert(room);
                 }
-                ants[ant] = node;
+                ants[ant] = room;
             }
         }
         true
